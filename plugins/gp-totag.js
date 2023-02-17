ae48d2de7d255f93a40b92a092fdd897d539ee65
martii.bs <117306268+Martinaa878@users.noreply.github.com>
2023-02-17 12:11:25 -0300

    Actualizar gp-totag.js

--- plugins/gp-totag.js
+++ plugins/gp-totag.js
@@ -6,9 +6,9 @@ let handler = async (m, { conn, text, participants}) => {
     conn.sendMessage(m.chat, { forward: m.quoted.fakeObj, mentions: users } )
 }
 
-handler.help = ['totag']
-handler.tags = ['group']
-handler.command = /^(totag|tag)$/i
+handler.help = ['notify']
+handler.tags = ['grupo']
+handler.command = /^(notify|tag)$/i
 
 handler.admin = true
 handler.group = true
